cc31f57f43971ebb253e01220fe5f93bcd7129fb
WalternativE <gregor@hakom.at>
2022-04-27 19:05:09 +0200

    [WIP] Adds experiments tuning lgbm models
    - Idea is that stumpf log regression works with very
      few samples as they are only used in callibration
    - In theory tuned lgbm should still outperform stumpf
      on train set of same size without overfitting for
      calibrated area of interest

--- eolearn_extras/ml_util.py
+++ eolearn_extras/ml_util.py
@@ -12,16 +12,36 @@ class AddValidTrainTestMasks(EOTask):
     def execute(self, eopatch: EOPatch):
         result_eop = eopatch.copy()
 
-        result_eop[(FeatureType.MASK_TIMELESS, 'train_split_valid')] = eopatch[self.train_test_maks_feature] & eopatch[self.valid_data_mask_feature]
+        bin_values = np.unique(result_eop[self.train_test_maks_feature])
+
+        train_marker = 1
+        if len(bin_values) == 2:
+            test_marker = 2
+        elif len(bin_values) == 3:
+            validation_marker = 2
+            test_marker = 3
+        else:
+            raise ValueError("Only supporting train/test or train/validation/test splits.")
+
         # multiplication by one to do an implicit type cast to a numerical value
-        result_eop[(FeatureType.MASK_TIMELESS, 'test_split_valid')] = ((eopatch[self.train_test_maks_feature] == 2) & (eopatch[self.valid_data_mask_feature] == 1)) * 1
+        result_eop[(FeatureType.MASK_TIMELESS, 'train_split_valid')] = ((result_eop[self.train_test_maks_feature] == train_marker ) & (result_eop[self.valid_data_mask_feature] == 1)) * 1
+        if len(bin_values) == 3:
+            result_eop[(FeatureType.MASK_TIMELESS, 'validation_split_valid')] = ((result_eop[self.train_test_maks_feature] == validation_marker) & (result_eop[self.valid_data_mask_feature] == 1)) * 1
+        # multiplication by one to do an implicit type cast to a numerical value
+        result_eop[(FeatureType.MASK_TIMELESS, 'test_split_valid')] = ((result_eop[self.train_test_maks_feature] == test_marker) & (result_eop[self.valid_data_mask_feature] == 1)) * 1
 
         traincount = np.sum(result_eop[(FeatureType.MASK_TIMELESS, 'train_split_valid')] == 1)
+        if len(bin_values) == 3:
+            validationcount = np.sum(result_eop[(FeatureType.MASK_TIMELESS, 'validation_split_valid')] == 1)
+        else:
+            validationcount = 0
         testcount = np.sum(result_eop[(FeatureType.MASK_TIMELESS, 'test_split_valid')] == 1)
 
         result_eop.meta_info['train_count'] = traincount
         result_eop.meta_info['test_count'] = testcount
-        result_eop.meta_info['train_perc'] = traincount / (traincount + testcount)
-        result_eop.meta_info['test_perc'] = testcount / (traincount + testcount)
+        result_eop.meta_info['validation_count'] = validationcount
+        result_eop.meta_info['train_perc'] = traincount / (traincount + testcount + validationcount)
+        result_eop.meta_info['test_perc'] = testcount / (traincount + testcount + validationcount)
+        result_eop.meta_info['validation_perc'] = validationcount / (traincount + testcount + validationcount)
 
         return result_eop
